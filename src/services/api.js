c63c778110931423040857e6ba52901443434858
JacobGeorgeMathew <152091838+JacobGeorgeMathew@users.noreply.github.com>
2025-07-28 19:06:44 +0530

    Made some changes and added .env file

--- src/services/api.js
+++ src/services/api.js
@@ -1,8 +1,9 @@
-const API_KEY = "a3e3b2515a2643a36b1a72bac31bde37";
+const API_KEY = import.meta.env.VITE_TMDB_API_KEY;
 const BASE_URL = "https://api.themoviedb.org/3";
 
 export const getPopularMovies = async () => {
   const response = await fetch(`${BASE_URL}/movie/popular?api_key=${API_KEY}`);
+ 
   const data = await response.json();
   return data.results;
 };
